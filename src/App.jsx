38e5088b67bffb710514dc9d90149e48bbb55113
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-02-19 00:45:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <Pet></Pet>
+
+    </>
+  )
+}
+function Pet()
+{
+  return (
+    <div>
+
+      <h2>CAT</h2>
+      <p>White color cat</p>
+    </div>
+    
+    
+    )
+}
+
+export default App
